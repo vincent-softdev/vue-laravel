d9e4100465984d999cdce27985d62bf9fb5e5f59
vincent-softdev <vinh.ly.softdev@gmail.com>
2025-03-27 01:35:17 +1030

    Update title for each route

--- e-commerce/frontend/src/main.js
+++ e-commerce/frontend/src/main.js
@@ -4,6 +4,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router/index'
 
+router.beforeEach((to, from, next) => {
+  const defaultTitle = 'Vincent Shop'
+  // setup title for each document
+  // Setup the title of the meta tag
+  document.title = to.meta.title || defaultTitle
+  // Proceed to the next route
+  next()
+  /*
+  next(false) will cancel the navigation
+  next('/login') Redirect to another route
+  next({path: '/something'}) Redirect using route object
+  */
+})
+
 createApp(App)
   .use(router)
   .mount('#app')
