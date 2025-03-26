698fc17855b606cb2fc3c59c6d97cc1651dc3681
vincent-softdev <vinh.ly.softdev@gmail.com>
2025-03-27 02:00:30 +1030

    update header nav

--- e-commerce/frontend/src/App.vue
+++ e-commerce/frontend/src/App.vue
@@ -1,13 +1,23 @@
 <script setup>
+import { ref } from 'vue'
 
+const isMenuOpen = ref(false)
 </script>
 
 <template>
 
   <div id="nav">
-    <router-link to="/">Home</router-link>
-    <router-link to="/my-images">My Images</router-link>
-    <router-link to="/login">Login</router-link>
+    <!-- Burger icon on small screens -->
+    <div class="burger" @click="isMenuOpen = !isMenuOpen">
+      ☰
+    </div>
+
+    <!-- Links wrapper -->
+    <div class="nav-links" :class="{ open: isMenuOpen }">
+      <router-link to="/" @click="isMenuOpen = false">Home</router-link>
+      <router-link to="/my-images" @click="isMenuOpen = false">My Images</router-link>
+      <router-link to="/login" @click="isMenuOpen = false">Login</router-link>
+    </div>
   </div>
   <!-- Body of the conten -->
   <div class="container">
