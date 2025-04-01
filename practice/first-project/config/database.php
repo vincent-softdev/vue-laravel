594a092e989e58c40e326ca6eccb9d1e76c5927c
vincent-softdev <vinh.ly.softdev@gmail.com>
2025-04-01 21:28:03 +1030

    fix DB

--- practice/first-project/config/database.php
+++ practice/first-project/config/database.php
@@ -33,13 +33,15 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            // 'url' => env('DB_URL'),
+            // 'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => database_path('database.sqlite'),
             'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-            'busy_timeout' => null,
-            'journal_mode' => null,
-            'synchronous' => null,
+            // 'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+            'foreign_key_constraints' => true,
+            // 'busy_timeout' => null,
+            // 'journal_mode' => null,
+            // 'synchronous' => null,
         ],
 
         'mysql' => [
